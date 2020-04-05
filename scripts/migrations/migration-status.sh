04626370429d5de8afeca79d3d6df2118672e3de
Artem Diashkin <nemrosim1988@gmail.com>
2020-04-05 12:26:16 +0300

    update migration script

--- scripts/migrations/migration-status.sh
+++ scripts/migrations/migration-status.sh
@@ -29,29 +29,38 @@ function is_second_param_valid {
     fi
 }
 
-run_command() {
+run_command(){
+  echo "Running migration:$1 on $2 environment"
+  echo
+    if [[ "$env_param" == ${PROD} ]]; then
+      cd ../.. && ENV_PATH=${PROD_ENV_FILE} adonis migration:$1
+    fi
+    if [[ "$env_param" == ${DEV} ]]; then
+      cd ../.. && ENV_PATH=${DEVELOPMENT_ENV_FILE} adonis migration:$1
+    fi
+    if [[ "$env_param" == ${TEST} ]]; then
+      cd ../.. && ENV_PATH=${TESTING_ENV_FILE} adonis migration:$1
+    fi
+}
+
+validate_and_run() {
   is_first_param_valid
   is_second_param_valid
 
   if [[ is_first_valid_result -eq 1 ]] && [[ is_second_valid_result -eq 1 ]]; then
-      echo
-      echo "Do you really want to run ${txBold}${txUnderline}$1${txReset} command on ${txBold}${txUnderline}$2${txReset} environment. Continue (yes/no)?"
-        read choice
 
-      if [[ "$choice" == "yes" ]]; then
-        echo "Running migration:$1 on $2 environment"
+      if [[ "$env_param" == ${PROD} ]];then
         echo
-          if [[ "$env_param" == ${PROD} ]]; then
-              cd ../.. && ENV_PATH=${PROD_ENV_FILE} adonis migration:$1
-          fi
-          if [[ "$env_param" == ${DEV} ]]; then
-              cd ../.. && ENV_PATH=${DEVELOPMENT_ENV_FILE} adonis migration:$1
-          fi
-          if [[ "$env_param" == ${TEST} ]]; then
-              cd ../.. && ENV_PATH=${TESTING_ENV_FILE} adonis migration:$1
+        echo "Do you really want to run ${txBold}${txUnderline}$1${txReset} command on ${txBold}${txUnderline}$2${txReset} environment. Continue (yes/no)?"
+          read choice
+
+          if [[ "$choice" == "yes" ]]; then
+            run_command ${command_param} ${env_param}
+          else
+            echo "You entered $choice. Script will not be executed"
           fi
       else
-        echo "You entered $choice. Script will not be executed"
+        run_command ${command_param} ${env_param}
       fi
   else
     echo
@@ -60,4 +69,4 @@ run_command() {
   fi
 }
 
-run_command ${command_param} ${env_param}
+validate_and_run ${command_param} ${env_param}
