e3240eac76e809fbf3a995f3d75d7e36c054ba30
Artem Diashkin <nemrosim1988@gmail.com>
2020-04-07 21:02:01 +0300

    add current user address controller

---
+++ app/Controllers/Http/CurrentUserAddressController.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const Address = use('App/Models/Address');
+
+class CurrentUserAddressController {
+    async index ({ auth }) {
+        return await Address.query()
+            .where('user_id', auth.user.id)
+            // .with('user')
+            .fetch();
+    }
+
+    async show ({ params }) {
+        const ADDRESS_ID = params.id;
+        const address = await Address.findBy('id', ADDRESS_ID);
+        if (address) {
+            return address;
+        }
+        return {
+            error: `Address not found by ID:${ADDRESS_ID}`,
+        };
+    }
+
+    async store ({ auth, request, response }) {
+        const { userId,
+            country,
+            regionState,
+            cityTown,
+            street,
+            house,
+            apartment,
+            postalCode } = request.post();
+
+        const address = new Address();
+
+        address.user_id = userId;
+        address.country = country;
+        address.region_state = regionState;
+        address.city_town = cityTown;
+        address.street = street;
+        address.house = house;
+        address.apartment = apartment;
+        address.postal_code = postalCode;
+
+        await address.save();
+    }
+
+
+    async update ({ request, params }) {
+        const user = await Address.find(params.id);
+
+        const { password, email } = request.post();
+
+        user.password = password;
+        user.email = email;
+
+        await user.save();
+    }
+
+
+    async destroy ({ params }) {
+        const ADDRESS_ID = params.id;
+
+        const address = await Address.findAllBy('user_id', ADDRESS_ID);
+        await address.delete();
+    }
+
+    async addressOfCurrentUser ({ auth }) {
+        return await Address.query()
+            .where('id', auth.user.id)
+            .with('profile')
+            .fetch();
+    }
+
+    async findByUserId ({ params }) {
+        const USER_ID = params.id;
+        const address = await Address.query()
+            .where('user_id', USER_ID)
+            .fetch();
+        if (address) {
+            return address;
+        }
+        return {
+            error: `Address not found by UserID:${USER_ID}`,
+        };
+    }
+
+    async deleteByUserId ({ params }) {
+        const USER_ID = params.id;
+        const address = await Address.query()
+            .where('user_id', USER_ID)
+            .fetch();
+        if (address.length > 0) {
+            const result = await Address.query()
+                .where('user_id', USER_ID)
+                .delete();
+            return {
+                message: `Deleted amount of addresses: ${result}`,
+            };
+        }
+        return {
+            error: `Addresses not found by UserID:${USER_ID}`,
+        };
+    }
+}
+
+module.exports = CurrentUserAddressController;
