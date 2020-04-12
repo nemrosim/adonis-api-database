293847f6b24a4152a7fa809631b2c49a87ce7f1a
Artem Diashkin <nemrosim1988@gmail.com>
2020-04-12 17:33:48 +0300

    add MailProvider to app providers

--- start/app.js
+++ start/app.js
@@ -7,6 +7,7 @@ const providers = [
     '@adonisjs/cors/providers/CorsProvider',
     '@adonisjs/lucid/providers/LucidProvider',
     '@adonisjs/validator/providers/ValidatorProvider',
+    '@adonisjs/mail/providers/MailProvider',
 ];
 
 /**
